8bf578c4c5eec20304aba3565a92cf860f3897d3
Antho <matvienko.anthony@gmail.com>
2022-04-25 16:09:59 +0200

    mail auto

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -32,8 +32,8 @@ function signUp() {
                 res.json()
                 .then((res) => {
                     //Send email auto
-                    sendEmailToCustomer(res.user.email);
-                    sendEmailToAdmin(res.user.email);
+                    sendEmailToCustomer(res.user);
+                    sendEmailToAdmin();
                     //Login auto after signup
                     fetch(`http://localhost:3000/api/mylaser/auth/login`, myInit)
                     .then(res => res.json())
@@ -52,12 +52,16 @@ function signUp() {
     })
 }
 
-function sendEmailToCustomer(mail) {
+function sendEmailToCustomer(user) {
     const mailInfos = {
-        email: mail,
-        subject: 'Nouveau compte',
-        text: 'Votre compte a bien été créé',
-        html: 'Votre compte a bien été créé'
+        name: user.firstName,
+        intro: 'Bienvenue chez MyLaser',
+        email: user.email,
+        subject: 'Bienvenue chez MyLaser',
+        instructions: 'Votre compte client a bien été créé. Vous pouvez désormais vous rendre sur votre espace client en vous connectant ici :',
+        text: 'Accéder à votre compte',
+        link: 'http://localhost:5501/my-account.html',
+        outro: 'L\'équipe MyLaser vous remercie et vous souhaite une agréable navigation'
     }
     const mailInit = {
         method: "POST",
@@ -72,12 +76,16 @@ function sendEmailToCustomer(mail) {
     })
 }
 
-function sendEmailToAdmin(mail) {
+function sendEmailToAdmin() {
     const mailInfos = {
-        email: mail,
-        subject: 'Nouveau compte client',
-        text: 'Un nouveau compte client a été créé',
-        html: 'Un nouveau compte client a été créé'
+        name: 'MyLaser',
+        intro: 'Un nouveau compte client a été créé !',
+        email: 'matvienko.anthony@gmail.com',
+        subject: 'Nouveaux Compte Client',
+        instructions: 'Vous pouvez le retrouver sur votre espace Admin en cliquant ici :',
+        text: 'Accéder à votre espace',
+        link: 'http://localhost:5501/admin-access-bo.html',
+        outro: 'A bientôt'
     }
     const mailInit = {
         method: "POST",
--- assets/js/success.js
+++ assets/js/success.js
@@ -4,15 +4,6 @@ const orders = document.getElementById('orders');
 const token = localStorage.getItem('customer');
 const decodedToken = jwt_decode(token);
 
-fetch(`http://localhost:3000/api/mylaser/user/${decodedToken.userId}`, {headers: {"Authorization": 'Bearer ' + token}})
-.then((res) => res.json())
-.then((user) => {
-    sendEmailToCustomer(user.email);
-    sendEmailToAdmin(user.email);
-})
-
-
-
 fetch(`http://localhost:3000/api/mylaser/payment/${session_id}`, {headers: {"Authorization": 'Bearer ' + token}})
 .then(res => {
     if(res.ok) {
@@ -20,6 +11,8 @@ fetch(`http://localhost:3000/api/mylaser/payment/${session_id}`, {headers: {"Aut
         localStorage.removeItem('currentQuote');
         res.json()
         .then(number => {
+            sendEmailToCustomer(number);
+            sendEmailToAdmin();
             orderNumber.innerHTML = number;
             const editPayment = {
                 payment: "Valid"
@@ -129,33 +122,45 @@ fetch(`http://localhost:3000/api/mylaser/payment/${session_id}`, {headers: {"Aut
     }
 })
 
-function sendEmailToCustomer(mail) {
-    const mailInfos = {
-        email: mail,
-        subject: 'Merci pour votre commande',
-        text: 'Merci pour votre commande',
-        html: 'Merci pour votre commande'
-    }
-    const mailInit = {
-        method: "POST",
-        body: JSON.stringify(mailInfos),
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-            "Authorization": 'Bearer ' + token,
-        },
-    }
-    fetch(`http://localhost:3000/api/mylaser/mail`, mailInit)
-    .then((res) => {
-        console.log(res)
+function sendEmailToCustomer(number) {
+    fetch(`http://localhost:3000/api/mylaser/user/${decodedToken.userId}`, {headers: {"Authorization": 'Bearer ' + token}})
+    .then((res) => res.json())
+    .then((user) => {
+        const mailInfos = {
+            name: user.firstName,
+            intro: `Nous vous confirmons votre commande N° : ${number}`,
+            email: user.email,
+            subject: `Confirmation de votre Commande N° : ${number}`,
+            instructions: 'Merci pour votre commande, elle sera traitée rapidement et vous serez informé de son état d\'avancement. Vous la retrouverez ici :',
+            text: 'Vos Commandes',
+            link: 'http://localhost:5501/my-orders.html',
+            outro: 'L\'équipe MyLaser vous remercie et vous souhaite une agréable navigation'
+        }
+        const mailInit = {
+            method: "POST",
+            body: JSON.stringify(mailInfos),
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "Authorization": 'Bearer ' + token,
+            },
+        }
+        fetch(`http://localhost:3000/api/mylaser/mail`, mailInit)
+        .then((res) => {
+            console.log(res)
+        })
     })
 }
 
-function sendEmailToAdmin(mail) {
+function sendEmailToAdmin() {
     const mailInfos = {
-        email: mail,
-        subject: 'Nouvelle commande',
-        text: 'Nouvelle commande',
-        html: 'Nouvelle commande'
+        name: 'MyLaser',
+        intro: 'Un nouvelle commande est arrivée!',
+        email: 'matvienko.anthony@gmail.com',
+        subject: 'Nouvelle Commande',
+        instructions: 'Vous pouvez la retrouver sur votre espace Admin en cliquant ici :',
+        text: 'Commandes',
+        link: 'http://localhost:5501/admin-access-bo-orders.html',
+        outro: 'A bientôt'
     }
     const mailInit = {
         method: "POST",
